83962806ea00f60ab0ebf55d9da90977530c70d3
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-07 22:11:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change the text of the main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+//move the image
+var img = document.elementById('maddy');
+img.onClick=function(){
+    img.style.marginLeft='100px';
+};
