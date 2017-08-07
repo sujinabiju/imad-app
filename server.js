d1a7d8a062ff05372bc719002a41aa876c0f5dd6
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-07 16:01:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/article-one',function(req,res){
     res.send("Article one requested and will be served here");
-})
+});
 app.get('/article-two',function(req,res){
     res.send("Article two requested and will be served here");
-})
+});
 app.get('/article-three',function(req,res){
     res.send("Article three requested and will be served here");
-})
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
