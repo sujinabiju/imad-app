5879e6a33e1d8cd2ec69e0460ee5e8295c1d7a7f
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-08 01:19:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,22 @@
 
 
+
+
+
+
 var button = document.getElementById('counter');
-
-button.onclick = function () {
-   
-    var request =new XMLHttpRequest();
-    request.onreadystatechange=function (){
-    
-     if(request.readystate === XMLHttpRequest.DONE){
-         if(request.status === 200){
-         var counter =  request.responseText;   
-         var span = document.getElementById('count');
-         
-         span.innerHTML=counter.toString();
-         
-         }
-     }
-     
-    };
-
-     request.open('GET','http://sujinabiju.imad.hasura-app.io/counter',true) ;
-     request.send(null);
-     
-    
+button.onclick = function(){
+    var request = new XMLHttpRequest();
+   request.onreadystatechange = function(){
+       if (httpRequest.readyState === XMLHttpRequest.DONE){
+           if (httpRequest.status === 200){
+               var counter = request.responseText;
+               var span= document.getElementById('count');
+               span.innerHTML = counter.toString();
+           }
+       }
+       
+   };
+    request.open('GET', 'http://http://sujinabiju.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
