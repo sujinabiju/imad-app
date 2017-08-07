ec15e23a752b159e97620ee062cd023bb2e17397
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-07 22:22:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 //move the image
 var img = document.getElementById('maddy');
-img.onClick=function() {
+img.onclick=function() {
     alert('jump');
     img.style.marginLeft='100px';
 };
