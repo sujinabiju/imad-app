9ccf4cc1ff7b5026a7a90813240cbd59b0e22098
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-08 00:57:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ button.onclick = function () {
          if(request.status === 200){
          var counter =  request.responseText;   
          var span = document.getElementById('count');
-         span.innerHTML=counter;
+         span.innerHTML=counter.toString();
          }
      }
      
