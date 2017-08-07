a639ea1e78b8f7a7275bf0d62a55d841ea42a30e
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-07 21:48:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-/*app.get('/ui/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});*/
+});
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
      res.send(createTemplate(articles[articleName]));
