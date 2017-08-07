c6c8acd497389871db7ef7f920b8364299f37d6b
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-08 01:19:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,6 @@ button.onclick = function(){
        }
        
    };
-    request.open('GET', 'http://http://sujinabiju.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://sujinabiju.imad.hasura-app.io/counter', true);
     request.send(null);
 };
