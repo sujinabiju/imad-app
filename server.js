c6c4f2443c8390c3ff2290901fe1d4cda66aae4e
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-10 09:12:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,9 +119,23 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     var articleName=req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
+
+    pool.query("SELECT * FROM article WHERE title= " + articleName , function(err,result){
+        if(err){
+            res.status(500).send(err.toString);
+        }
+        else{
+        if(result.rows.length === 0){
+          res.status(404).send('Article not found') ; 
+        }else{
+           var articleData=result.rows[0];
+           res.send(createTemplate(articles[articleData]));
+        }
+        }
+    });
+     
 });
 
 app.get('/ui/madi.png', function (req, res) {
