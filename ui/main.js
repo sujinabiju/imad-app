db11aa678986681047b2aec74ead300c2b6df2d9
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-26 19:04:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -76,7 +76,7 @@ submit.onclick = function(){
             console.log(username);
             console.log(password);
             request.open('POST','http://sujinabiju.imad.hasura-app.io/login',true);
-            request.setReqestHeader('Content-Type','application/json');
+            request.setRequestHeader('Content-Type','application/json');
             request.send(JSON.stringify({username:username,password:password}));
            };
    
