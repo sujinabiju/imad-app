e636801597776b8fba7c013a5e4350d118dc9e6c
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-26 19:30:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var config = {
     user: 'sujinabiju',
     database: 'sujinabiju',
@@ -14,7 +15,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret:'someRandomSecretValue',
+    cookie : {maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 var articles={
   'article-one':{
@@ -127,6 +131,7 @@ app.post('/login',function(req,res){
              var salt = dbString.split('$')[2];
              var hashedPassword = hash(password,salt);
              if(hashedPassword ===dbString){
+                 req.session.auth ={userId: result.rows[0].id};
                  res.send('credentials are correct');
              }else{
                 res.status(403).send('username/password is invalid');
@@ -135,6 +140,14 @@ app.post('/login',function(req,res){
      }  
     });
     
+});
+app.get('/check-login',function(req,res){
+    if(req.session && req.session.auth && req.session.auth.uerId){
+        res.send('You are logged in: ' + req.session.auth.userId.tostring());
+    }
+    else{
+        res.send('you are not logged in');
+    }
 });
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
