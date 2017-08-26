e7d9dcc061177f1fd19be6d9513a119388dffdb9
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-26 19:18:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,7 +57,7 @@ submit.onclick = function(){
            {
             var request = new XMLHttpRequest();
             request.onreadystatechange = function(){
-                if(request.readystate === XMLHttpRequest.DONE){
+                if(request.readyState === XMLHttpRequest.DONE){
                     if(request.status === 200){
                         console.log('user logged in');
                         alert('Logged in successfully');
