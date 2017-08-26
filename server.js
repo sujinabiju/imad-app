b7059c57f8a5cb53ee89d2855738d2ffb6bfbb3e
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2017-08-26 13:48:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user: 'sujinabiju',
     database: 'sujinabiju',
@@ -84,6 +85,14 @@ var htmlTemplate=`
 `;
 return htmlTemplate;
 }
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+   var hashedString = hash(req.params.input,'this-is-some-random-string') ;
+   res.send(hashedString);
+});
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query('SELECT * FROM test',function(err,result){
